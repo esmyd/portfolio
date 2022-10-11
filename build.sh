77df3b0f383b7e03a92abeda533eb9d91c963684
esmyd <tecnologicosiglo@gmail.com>
2022-10-11 12:31:32 -0500

    disable ejecutable2 3

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 # poetry install
-pip install -r requirements.txt
-/opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
+pip install -r requirements.txt 
+/opt/render/project/src/.venv/bin/python3.9 -m pip install --upgrade pip
 python manage.py collectstatic --no-input
 python manage.py migrate
