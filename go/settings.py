db1d0e7802decfbc01ab532b77980adf5a79f9df
esmyd <tecnologicosiglo@gmail.com>
2022-10-18 14:03:22 -0500

    disable ejecutable2 30

--- go/settings.py
+++ go/settings.py
@@ -119,14 +119,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-# Following settings only make sense on production and may break development environments.
-if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
-    # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Turn on WhiteNoise storage backend that takes care of compressing static files
-    # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storages.backends.s3boto3.S3Boto3Storage'
-    
+DEBUG = False
 MEDIA_ROOT = BASE_DIR / "media"
 
 MEDIA_URL = "/media/"
