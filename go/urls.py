89d9b7e1ac0668234fd9141b0bf88c91e81e29a6
esmyd <tecnologicosiglo@gmail.com>
2022-10-21 17:01:30 -0500

    configuracion con static6

--- go/urls.py
+++ go/urls.py
@@ -11,4 +11,4 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("blog/", include('blog.urls')),
 ]
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
