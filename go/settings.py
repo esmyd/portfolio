1da5637d0c25441363e6182963769d18c84429c1
esmyd <tecnologicosiglo@gmail.com>
2022-10-21 17:38:57 -0500

    configuracion con static8

--- go/settings.py
+++ go/settings.py
@@ -115,10 +115,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, '/go/static/')
-STATIC_DIRS = [
-    os.path.join(BASE_DIR, '/static/'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, '/home/gosorio/portfolio/portfolio/portfolio/static')
+# Following settings only make sense on production and may break development environments.
+if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, '/home/gosorio/portfolio/portfolio/portfolio/static')
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storages.backends.s3boto3.S3Boto3Storage'
     
 MEDIA_ROOT = BASE_DIR / "media"
 
