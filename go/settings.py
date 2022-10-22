9a5cec13904e5bb3aab2a41107f18701d18ad351
esmyd <tecnologicosiglo@gmail.com>
2022-10-22 11:51:48 -0500

    configuracion con static11

--- go/settings.py
+++ go/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Following settings only make sense on production and may break development environments.
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
