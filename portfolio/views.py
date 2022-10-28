96f86664b2ca61ee9897e522e91ce415ddf22e3c
esmyd <tecnologicosiglo@gmail.com>
2022-10-28 17:49:17 -0500

    ordenar proyectos

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,7 +3,7 @@ from .models import Project, Profile
 
 
 def home(request):
-    project_web = Project.objects.filter(type_proyect=1)
-    project_app = Project.objects.filter(type_proyect=2)
+    project_web = Project.objects.filter(type_proyect=1).order_by("date")
+    project_app = Project.objects.filter(type_proyect=2).order_by("date")
     profile = Profile.objects.first()
     return render(request, 'home.html', {'project_web': project_web, 'project_app': project_app, 'profile': profile })
